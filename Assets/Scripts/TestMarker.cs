43c7b7432cb98fc6dfbce58384e957a11ef14f38
Yu-Jen Lin <yjlin5210@gmail.com>
2017-03-31 20:12:46 -0400

    Update example code

---
+++ Assets/Scripts/TestMarker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMarker : MarkerGO {
+
+	Renderer m_Renderer;
+	// Use this for initialization
+	void Start () {
+		m_Renderer = gameObject.GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (highlight) {
+			m_Renderer.material.color = new Color32(200, 0, 0, 255);
+		} else {
+			m_Renderer.material.color = new Color32(255, 255, 255, 255);
+		}
+	}
+}
